a647496ee8083e24fa407128b3217adb53b18d3e
Shchuchinov.Ruslan <Ruslan.Shchuchinov@infotecs.ru>
2020-11-09 19:11:11 +0300

    Небольшие правки

--- main.py
+++ main.py
@@ -18,20 +18,22 @@ def derivation(x, function):
 # Задание 2:
 # посчитать значение градиента функции $x_1^2\cos(x_2) + 0.05x_2^3 + 3x_1^3\log_2{x_2^2}$ в точке $(10, 1)$.
 def function2(point):
-    return (point[0]**2) * math.cos(point[1]) + 0.05 * (point[1]**3) + 3*(point[0]**3) * math.log(point[1]**2, 2)
+    x = point[0]
+    y = point[1]
+    return (x**2) * math.cos(y) + 0.05 * (x**3) + 3*(x**3) * math.log(y**2, 2)
 
 
 def gradient(point, function):
     x = point[0]
     y = point[1]
+    fxy = function([x, y])
 
-    dx = 0.0000001
-    df_dx = (function([x + dx, y]) - function([x, y])) / dx
+    dx = 0.000001
+    df_dx = (function([x + dx, y]) - fxy) / dx
+    dy = 0.000001
+    df_dy = (function([x, y + dy]) - fxy) / dy
 
-    dy = 0.0000001
-    df_dy = (function([x, y + dy]) - function([x, y])) / dy
-    result = math.sqrt(df_dx**2 + df_dy**2)
-    return round(result, 2)
+    return [round(df_dx, 2), round(df_dy, 2)]
 
 
 # Задание 3:
@@ -71,6 +73,8 @@ def gradient_optimization_multi_dim(function):
         df_dy = (function([x, y + dy]) - function([x, y])) / dy
         x = x - epsilon * df_dx
         y = y - epsilon * df_dy
+    x = round(x, 2)
+    y = round(y, 2)
     return [x, y]
 
 
